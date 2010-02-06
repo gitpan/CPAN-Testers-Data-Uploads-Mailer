c5e27cdcb0e47fd8d24ba4c3e899cf679cf49b9b
Barbie <barbie@missbarbell.co.uk>
2010-02-06 09:56:26 +0000

    Import of BARBIE/CPAN-Testers-Data-Uploads-Mailer-0.02 from CPAN.
    gitpan-cpan-distribution: CPAN-Testers-Data-Uploads-Mailer
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BARBIE/CPAN-Testers-Data-Uploads-Mailer-0.02.tar.gz
    gitpan-cpan-author:       BARBIE
    gitpan-cpan-maturity:     released

--- lib/CPAN/Testers/Data/Uploads/Mailer.pm
+++ lib/CPAN/Testers/Data/Uploads/Mailer.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 $|++;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 #----------------------------------------------------------------------------
 
@@ -42,6 +42,7 @@ use Time::Piece;
 
 # the following will have the emails mailed to them too
 my @ADMINS = ('barbie@missbarbell.co.uk');
+my %ADMINS = map {$_ => 1} @ADMINS;
 
 my %default = (
     source      => 'logs/uploads.log',
@@ -65,10 +66,13 @@ Date: DATE
 
 sub new {
     my $class = shift;
+    my %opts  = @_;
 
     my $self = {};
     bless $self, $class;
 
+    $self->{options} = {};
+    $self->{default}{$_} = $self->_defined_or($opts{$_}, $default{$_})  for(keys %default);
     $self->_init_options(@_);
     return $self;
 }
@@ -79,6 +83,8 @@ sub process {
     my $lastid  = $self->_last_id();
     my $last_id = $lastid;
 
+    $self->{mail}{layout}  = 'mail-bad-uploads.eml';
+
     my $fh = IO::File->new($source) or die "Cannot open file [$source]: $!";
     while(<$fh>) {
         chomp;
@@ -86,23 +92,24 @@ sub process {
         #... [1281307] subject=CPAN Upload: A/AP/APLA/update_db_schema.pl
         my ($id,$path,$cpan,$dist) = m!\.\.\. \[(\d+)\] subject=CPAN Upload: (\w/\w{2}/(\w+)/(.*))!;
         next    unless($id && $id > $lastid);
+        $last_id = $id;
 
         next    unless(defined $cpan);
-        next    unless($dist =~ /\.(zip|tar|pm|gz)/);   # only attempts caught for now
-        next    if($dist =~ /\.(asc|pdf|ppm|patch)/);   # docs and patches, etc. are fine
+        next    if($dist =~ /\.(?:(?:tar\.|t)(?:gz|bz2)|zip)$/i);   # valid archives
+        next    if($dist =~ /\.(pl|sh)/i);                          # ignore scripts ...
+        next    if($dist =~ /\.(gif|png|jpg)/i);                    # ... images ...
+                                                                    # ... docs and patches, etc.
+        next    if($dist =~ /\.(asc|pdf|ppm|patch|readme|meta|yml|pod|txt|changelog)/i);
 
+        if($dist !~ /\b(rar|tgs|tbz|zip|tar|pm|gz|bz2|tz)$/i) {     # only attempts caught for now
+            $self->{mail}{others} .= "$id,$path\n";
+            next;
+        }
         $self->{mail}{authors}{$cpan.'@cpan.org'} = 1;
-        $self->{mail}{uploads} .= "$path\n";
-
-        $last_id = $id;
+        $self->{mail}{uploads} .= "$id,$path\n";
     }
 
-    $self->_send_mail(
-        authors => $self->{mail}{authors},
-        subject => 'Non-Standard Formatted CPAN Uploads',
-        layout  => 'mail-bad-uploads.eml',
-        reports => $self->{mail}{uploads}
-    );
+    $self->_send_mail();
 
     $self->_last_id($last_id);
 }
@@ -112,47 +119,48 @@ sub process {
 
 sub _send_mail {
     my $self = shift;
-    my %hash = @_;
 
-    return  unless(defined $hash{authors} && keys %{$hash{authors}});
+    return  unless(defined $self->{mail}{authors} && keys %{$self->{mail}{authors}});
 
     my $DATE = _emaildate();
     $DATE =~ s/\s+$//;
 
-    my $BODY = $HEAD . _create_mail($hash{layout},$hash{reports});
+    my %tvars = (
+        date    => $DATE,
+        uploads => $self->{mail}{uploads},
+    );
 
-    my @recipients;
-    push @recipients, (keys %{$hash{authors}}) unless($self->{options}{test});
+    my   @recipients;
+    push @recipients, (keys %{$self->{mail}{authors}}) unless($self->{options}{test});
     push @recipients, @ADMINS;
 
     for my $addr (@recipients) {
-        my $body = $BODY;
-        $body =~ s/EMAIL/$addr/g;
-        $body =~ s/DATE/$DATE/g;
-        $body =~ s/SUBJECT/$hash{subject}/g;
+        $tvars{email}  = $addr;
+        $tvars{others} = $ADMINS{$addr} ? $self->{mail}{others}||'' : '';
+
+        my $body = _create_mail($self->{mail}{layout},\%tvars);
 
         my $cmd = qq!| $HOW $addr!;
 
         if($self->{options}{debug}) {
-                _log("$DATE: NULL: $addr [$hash{subject}]");
+                $self->_log("$DATE: NULL: $addr");
+                $self->_log("$body");
         } else {
             if(my $fh = IO::File->new($cmd)) {
                 print $fh $body;
                 $fh->close;
-                _log("$DATE: PASS: $addr [$hash{subject}]");
+                $self->_log("$DATE: PASS: $addr");
             } else {
-                _log("$DATE: FAIL: $addr [$hash{subject}]");
+                $self->_log("$DATE: FAIL: $addr");
             }
         }
     }
 }
 
 sub _create_mail {
-    my $layout  = shift;
-    my $content = shift;
+    my $layout = shift;
+    my $tvars  = shift;
     my $body;
-
-    my %tvars = ( content => $content );
 
     my %config = (                              # provide config info
         RELATIVE        => 1,
@@ -164,7 +172,7 @@ sub _create_mail {
     );
 
     my $parser = Template->new(\%config);       # initialise parser
-    $parser->process($layout,\%tvars,\$body)    # parse the template
+    $parser->process($layout,$tvars,\$body)     # parse the template
         or die $parser->error();
 
     return $body;
@@ -183,7 +191,7 @@ sub _last_id {
 
     overwrite_file( $self->{options}{lastfile}, 0 ) unless -f $self->{options}{lastfile};
 
-    if ($id) {
+    if (defined $id) {
         overwrite_file( $self->{options}{lastfile}, $id );
     } else {
         $id = read_file($self->{options}{lastfile});
@@ -203,16 +211,25 @@ sub _log {
     $fh->close;
 }
 
+sub _defined_or {
+    my $self = shift;
+    while(@_) {
+        my $value = shift;
+        return $value   if(defined $value);
+    }
+
+    return;
+}
+
 sub _init_options {
     my $self = shift;
-    $self->{options} = {};
 
     GetOptions( $self->{options},
         'source|s=s',
         'logfile=s',
         'lastfile=s',
-        'test|t',
-        'debug|d',
+        'test|t!',
+        'debug|d!',
         'help|h',
         'version|v'
     );
@@ -220,15 +237,15 @@ sub _init_options {
     _help(1) if($self->{options}{help});
     _help(0) if($self->{options}{version});
 
-    $self->{options}{$_} ||= $default{$_}   for(qw(logfile source lastfile test debug));
+    $self->{options}{$_} = $self->_defined_or($self->{options}{$_}, $self->{default}{$_})  for(keys %default);
 
-    unless(-f $self->{options}{log}) {
-        print "No cpanstats.log file [$self->{options}{log}] found\n\n";
+    unless(-f $self->{options}{source}) {
+        print "No uploads source log file [$self->{options}{source}] found\n\n";
         _help(1);
     }
 
-    mkpath(dirname($self->{options}{last}));
-    mkpath(dirname($self->{options}{out}));
+    mkpath(dirname($self->{options}{lastfile}));
+    mkpath(dirname($self->{options}{logfile}));
 }
 
 sub _help {
@@ -238,12 +255,12 @@ sub _help {
         print <<HERE;
 
 Usage: $0 \\
-        [--log=<file>] [--out=<file>] [--last=<file>] \\
+        [--logfile=<file>] [--source=<file>] [--lastfile=<file>] \\
         [--test] [--debug] [-h] [-V]
 
-  --log             log file from cpanstats-verify
-  --out             results output file
-  --last            last NNTP ID mailed out
+  --logfile         log file from cpanstats-verify
+  --source          results output file
+  --lastfile        last NNTP ID mailed out
   --test            send mails to admin only
   --debug           do not send mails
   -h                this help screen
@@ -297,9 +314,12 @@ be forthcoming, please feel free to (politely) remind me.
 
 =head1 SEE ALSO
 
-L<IO::File>
+L<CPAN::Testers::Data::Uploads>
 
-F<http://stats.cpantesters.org/>
+F<http://www.cpantesters.org/> (Reports),
+F<http://stats.cpantesters.org/> (Statistics),
+F<http://wiki.cpantesters.org/> (Wiki),
+F<http://devel.cpantesters.org/> (Development)
 
 =head1 AUTHOR
 
